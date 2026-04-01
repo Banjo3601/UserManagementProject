02332ba3707116e82fbae9a79ac1adf403d61549
Banjo3601 <paolo30500@gmail.com>
2026-04-01 23:59:36 +0200

    Ajustement titre

--- UserManagementApi/obj/Debug/net10.0/UserManagementApi.AssemblyInfo.cs
+++ UserManagementApi/obj/Debug/net10.0/UserManagementApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UserManagementApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4cbb4b19ec7403f60558ca1e703142a95f67bc7a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c84cdaa468051451ef3e25324e1998126a971478")]
 [assembly: System.Reflection.AssemblyProductAttribute("UserManagementApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UserManagementApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
